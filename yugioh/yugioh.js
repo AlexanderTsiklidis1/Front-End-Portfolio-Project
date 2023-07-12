24f2438263213798c5b982cac5f973df6cd3c2f8
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-07-12 18:35:02 -0400

    outlines goals of projects in commeents, imports guideline code for project

--- yugioh/yugioh.js
+++ yugioh/yugioh.js
@@ -1,3 +1,9 @@
+//Need to use this code below from class notes to 
+//generate similar sort of form to display yugioh cards by a particular inputted key
+//will need to rename variables
+
+
+
 // button.addEventListener("click", (e) => {
 //     e.preventDefault();
 //     const id = input.value;
