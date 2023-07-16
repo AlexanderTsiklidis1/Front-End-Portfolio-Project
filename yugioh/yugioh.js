25c9edb82a7556db2ce9e90b91cb8d9ce83f5939
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-07-16 09:12:47 -0400

    completes most of project

--- yugioh/yugioh.js
+++ yugioh/yugioh.js
@@ -5,7 +5,7 @@ let form = document.querySelector('form')
 let url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=";
 let url1 = "https://db.ygoprodeck.com/api/v7/cardinfo.php?id=";
 
-searchName.addEventListener('keyup', (event) => {
+searchName.addEventListener('click', (event) => {
 	if (event.key !== 'Enter') return;
 	searchButton.click(); 
 	event.preventDefault(); 
